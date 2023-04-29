2328a006693248c43dcc2dc99050fd38d862709f
Kjetil Valstadsve <kjetilv.github@vlitejk.cotse.net>
2023-04-29 13:34:25 +0200

    Ensure values

--- uplift-kernel/src/main/java/com/github/kjetilv/uplift/kernel/aws/AwsAuth.java
+++ uplift-kernel/src/main/java/com/github/kjetilv/uplift/kernel/aws/AwsAuth.java
@@ -1,4 +1,11 @@
 package com.github.kjetilv.uplift.kernel.aws;
 
+import java.util.Objects;
+
 public record AwsAuth(String key, String secret) {
+
+    public AwsAuth {
+        Objects.requireNonNull(key, "key");
+        Objects.requireNonNull(secret, "secret");
+    }
 }
