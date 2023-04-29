238876532401508e0d801910ab60d41c93b75f7c
Kjetil Valstadsve <kjetilv.github@vlitejk.cotse.net>
2023-04-29 13:33:45 +0200

    Disable test.

--- uplift-kernel/src/test/java/com/github/kjetilv/uplift/kernel/aws/AwsLookupTest.java
+++ uplift-kernel/src/test/java/com/github/kjetilv/uplift/kernel/aws/AwsLookupTest.java
@@ -1,11 +1,13 @@
 package com.github.kjetilv.uplift.kernel.aws;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class AwsLookupTest {
 
+    @Disabled
     @Test
     void shouldFindDefault() {
         assertThat(AwsLookup.get("default")).isPresent();
