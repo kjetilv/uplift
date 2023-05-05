1c51cb66aa50fd255acef3bcb8181f3a6e1167e4
Kjetil Valstadsve <kjetilv.github@vlitejk.cotse.net>
2023-05-05 18:47:35 +0200

    Admonitions

--- examples/hello-web/hello-web-uplift/build.gradle.kts
+++ examples/hello-web/hello-web-uplift/build.gradle.kts
@@ -14,12 +14,15 @@ dependencies {
 apply<UpliftPlugin>()
 
 tasks.withType<UpliftTask> {
-// Uncomment and fix, or provide suitable gradle.properties
+
+    admonish() // TODO Remove this when you have your properties in order
+
+// // TODO One way to do it: Uncomment and fix, or provide suitable gradle.properties
 //    configure(
-//        account = "...",
-//        region = "mars-east-1",
-//        profile = "default",
-//        stack = "mystack"
+//        account = "<your 12-digit AWS account id>"",
+//        region = "<your preferred region>",
+//        profile = "default", // This is the default
+//        stack = hello-web-hello-web-uplift" // This is the silly default
 //    )
 //    env(
 //        "FOO" to "bar"
@@ -33,3 +36,31 @@ tasks.withType<UpliftTask> {
     )
 }
 
+fun admonish() {
+    listOf("account", "region").associateWith { prop ->
+        try {
+            project.property(prop).toString()
+        } catch (e: Exception) {
+            "".also {
+                logger.error("Missing property $prop, deploy step will fail:\n $e")
+            }
+        }
+    }.also { map ->
+        val missing = map.entries.filter { it.value.isBlank() }
+            .takeIf { it.isNotEmpty() }
+            ?.let { list ->
+                logger.error("\nMISSING SETUP FOR UPLIFT\n\nYou need to provide value${
+                    if (list.size > 1) "s" else ""
+                } for propert${
+                    if (list.size > 1) "ies" else "y"
+                } ${
+                    list.joinToString { "`${it.key}`" }
+                }!\n\nTo set, either plug them right into this build file, or set them as gradle properties.\n" +
+                        "(E.g. add a gradle.properties file in ${System.getProperty("user.dir")})\n" +
+                        "(And then, remove the method admonishing you like this.)")
+            }
+        if (missing == null) {
+            logger.info("Great! Now you can remove this nasty method")
+        }
+    }
+}
