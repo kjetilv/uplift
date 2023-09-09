dd8e5979bacbb0bbbe600cbd51875a05849f50b7
Kjetil Valstadsve <kjetilv.github@vlitejk.cotse.net>
2023-09-09 12:29:08 +0200

    Optimize

--- uplift-json/build.gradle.kts
+++ uplift-json/build.gradle.kts
@@ -1,7 +1,4 @@
 dependencies {
-    testImplementation("com.fasterxml.jackson.core:jackson-core:2.15.2")
-    testImplementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
-
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
     testImplementation("org.assertj:assertj-core:3.24.2")
