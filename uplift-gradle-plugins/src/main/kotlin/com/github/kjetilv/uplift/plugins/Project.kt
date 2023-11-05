1453695364608d6352be1353b2f0b263d0a691ac
Kjetil Valstadsve <kjetilv.github@vlitejk.cotse.net>
2023-11-05 09:32:18 +0100

    Out of the shadows, into the light-weight

--- uplift-gradle-plugins/src/main/kotlin/com/github/kjetilv/uplift/plugins/Project.kt
+++ uplift-gradle-plugins/src/main/kotlin/com/github/kjetilv/uplift/plugins/Project.kt
@@ -4,8 +4,8 @@ import org.gradle.api.Project
 import java.nio.file.Files
 import java.nio.file.Path
 
-internal fun Project.cdkApp() =
-    buildDir.toPath().resolve("cdk-app").also(Files::createDirectories)
+internal fun Project.cdkApp(): Path =
+    layout.buildDirectory.dir("cdk-app").get().asFile.toPath().also(Files::createDirectories)
 
 internal fun Project.templateFile(task: UpliftTask): Path =
     cdkApp().resolve("cdk.out").resolve("${task.stack.get()}.template.json")
