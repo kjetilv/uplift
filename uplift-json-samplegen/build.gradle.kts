f6517480b49cd2f27a5ee557e3a7a870c15e6286
Kjetil Valstadsve <kjetilv.github@vlitejk.cotse.net>
2025-07-22 11:13:43 +0200

    Versions

--- uplift-json-samplegen/build.gradle.kts
+++ uplift-json-samplegen/build.gradle.kts
@@ -4,7 +4,6 @@ dependencies {
     implementation("com.github.kjetilv.flopp:flopp-kernel:0.1.0-SNAPSHOT")
     annotationProcessor(project(":uplift-json"))
 
-    testImplementation(project(":uplift-json-samplegen"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.13.3")
     testImplementation("org.junit.jupiter:junit-jupiter:5.13.3")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
