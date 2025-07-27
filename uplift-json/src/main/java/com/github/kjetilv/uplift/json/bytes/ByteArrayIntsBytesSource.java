81e329a365fb7a4230b4b32603b5b750c8f3392d
Kjetil Valstadsve <kjetilv.github@vlitejk.cotse.net>
2025-07-27 11:51:25 +0200

    Naming

--- uplift-json/src/main/java/com/github/kjetilv/uplift/json/bytes/ByteArrayIntsBytesSource.java
+++ uplift-json/src/main/java/com/github/kjetilv/uplift/json/bytes/ByteArrayIntsBytesSource.java
@@ -1,14 +1,15 @@
 package com.github.kjetilv.uplift.json.bytes;
 
 import java.util.Objects;
+import java.util.function.IntSupplier;
 
 public final class ByteArrayIntsBytesSource extends AbstractIntsBytesSource {
 
     public ByteArrayIntsBytesSource(byte[] bytes) {
-        super(new IntSupplier(Objects.requireNonNull(bytes, "bytes")));
+        super(new Ints(Objects.requireNonNull(bytes, "bytes")));
     }
 
-    private static class IntSupplier implements java.util.function.IntSupplier {
+    private static class Ints implements IntSupplier {
 
         private int i;
 
@@ -16,7 +17,7 @@ public final class ByteArrayIntsBytesSource extends AbstractIntsBytesSource {
 
         private final byte[] bytes;
 
-        public IntSupplier(byte[] bytes) {
+        public Ints(byte[] bytes) {
             this.bytes = bytes;
             i = 0;
             len = bytes.length;
