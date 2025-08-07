747c30b43fc97966e68e40671e580e72f7d9c1a1
Kjetil Valstadsve <kjetilv.github@vlitejk.cotse.net>
2025-08-07 18:32:19 +0200

    gen module

---
+++ uplift-json-gen/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module uplift.json.gen {
+
+    requires java.compiler;
+    requires uplift.uuid;
+    requires uplift.json;
+    requires uplift.kernel;
+
+    exports com.github.kjetilv.uplift.json.gen;
+    exports com.github.kjetilv.uplift.json.anno;
+}
