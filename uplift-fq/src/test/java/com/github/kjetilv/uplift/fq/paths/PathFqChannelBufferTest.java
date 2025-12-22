3c4f6a698046f385a7c6357d3940329792939c89
Kjetil Valstadsve <kjetilv.github@vlitejk.cotse.net>
2025-12-22 21:07:16 +0100

    Buffers

--- uplift-fq/src/test/java/com/github/kjetilv/uplift/fq/paths/PathFqChannelBufferTest.java
+++ uplift-fq/src/test/java/com/github/kjetilv/uplift/fq/paths/PathFqChannelBufferTest.java
@@ -20,7 +20,6 @@ import java.util.stream.Stream;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.io.CleanupMode.ON_SUCCESS;
 
-@Disabled
 class PathFqChannelBufferTest {
 
     @Test
