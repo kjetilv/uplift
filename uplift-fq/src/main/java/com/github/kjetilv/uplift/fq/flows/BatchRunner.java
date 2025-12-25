bbab853a3e4bc9fb74b62f7282bda1f6a353b05f
Kjetil Valstadsve <kjetilv.github@vlitejk.cotse.net>
2025-12-25 20:12:34 +0100

    vars

--- uplift-fq/src/main/java/com/github/kjetilv/uplift/fq/flows/BatchRunner.java
+++ uplift-fq/src/main/java/com/github/kjetilv/uplift/fq/flows/BatchRunner.java
@@ -2,6 +2,7 @@ package com.github.kjetilv.uplift.fq.flows;
 
 import com.github.kjetilv.uplift.fq.Fqs;
 
+import java.util.List;
 import java.util.stream.Stream;
 
 final class BatchRunner<T> extends AbstractRunner<T> {
@@ -18,22 +19,35 @@ final class BatchRunner<T> extends AbstractRunner<T> {
     }
 
     @Override
-    protected Stream<Entries<T>> entries(Name source, Fqs<T> fqs, Flow<T> flow, FqFlows.ErrorHandler<T> handler) {
+    protected Stream<Entries<T>> entries(
+        Name source,
+        Fqs<T> fqs,
+        Flow<T> flow,
+        FqFlows.ErrorHandler<T> handler
+    ) {
         return fqs.batches(source, batchSize)
-            .map(items -> {
-                var entries = Entries.of(flow.name(), items);
-                Entries<T> processed;
-                try {
-                    processed = flow.processor().process(entries);
-                } catch (Exception e) {
-                    processed = entries.map(t ->
-                        handler.failed(flow, t, e)
-                    );
-                }
-                if (processed.matches(entries)) {
-                    return processed;
-                }
-                throw new IllegalStateException("Expected " + items.size() + " items, got " + processed.size());
-            });
+            .map(items ->
+                entries(flow, items))
+            .map(entries ->
+                process(flow, entries, handler)
+            );
+    }
+
+    private static <T> Entries<T> entries(Flow<T> flow, List<T> items) {
+        return Entries.of(flow.name(), items);
+    }
+
+    private static <T> Entries<T> process(
+        Flow<T> flow,
+        Entries<T> entries,
+        FqFlows.ErrorHandler<T> handler
+    ) {
+        try {
+            return flow.processor().process(entries);
+        } catch (Exception e) {
+            return entries.map(t ->
+                handler.failed(flow, t, e)
+            );
+        }
     }
 }
--- uplift-fq/src/main/java/com/github/kjetilv/uplift/fq/flows/SingleRunner.java
+++ uplift-fq/src/main/java/com/github/kjetilv/uplift/fq/flows/SingleRunner.java
@@ -19,13 +19,19 @@ final class SingleRunner<T> extends AbstractRunner<T> {
     ) {
         return fqs.streamer(source)
             .read()
-            .map(item -> {
-                var entry = Entries.single(flow.to(), item);
+            .map(item ->
+                entry(flow, item))
+            .map(entry -> {
                 try {
                     return flow.processor().process(entry);
                 } catch (Exception e) {
-                    return entry.map(t -> handler.failed(flow, t, e));
+                    return entry.map(t ->
+                        handler.failed(flow, t, e));
                 }
             });
+    }
+
+    private static <T> Entries<T> entry(Flow<T> flow, T item) {
+        return Entries.single(flow.to(), item);
     }
 }
